5dfae9374f305639237d8e3c8fcf13d08077cf06
liamfletcher <liam.fletcher@ocsbstudent.ca>
2020-12-15 17:01:39 +0000

    Everything

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function addNumbers () {
   const a = +document.getElementById('x1').value
   const b = +document.getElementById('x2').value
   const h = +document.getElementById('x3').value
-  const output = (a+b)/2 * h 
-  document.getElementById('td').innerHTML=output
+  const output = (a + b) / 2 * h
+  document.getElementById('td').innerHTML = output
 }
